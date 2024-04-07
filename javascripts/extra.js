41a844ee93d36cee1095b1b2b6a52fc6332fe40e
Dominik Parus <dom7575@o2.pl>
2024-04-07 17:04:32 +0200

    Add files via upload

---
+++ javascripts/extra.js
@@ -0,0 +1,42 @@
+var astral = document.querySelector('[src="../img/BuzzSC.png"]');
+var zurg = document.querySelector('[src="../img/Zurg.png"]');
+var forky = document.querySelector('[src="../img/Forky.png"]');
+var rex = document.querySelector('[src="../img/Rex.png"]');
+var bulwa = document.querySelector('[src="../img/Pan%20Bulwa.png"]');
+var czarny = document.querySelector('[src="../img/Czarny.png"]');
+
+if (astral) {
+    astral.addEventListener("click", function() {
+        alert('Na koniec świata i jeszcze dalej! 🚀');
+    });
+}
+
+if (zurg) {
+    zurg.addEventListener("click", function() {
+        alert('Zniszczyć Buzza Astrala! Zniszczyć Buzza Astrala! Zniszczyć Buzza Astrala!');
+    });
+}
+
+if (forky) {
+    forky.addEventListener("click", function() {
+        alert('Jestem śmieciem! 🗑️');
+    });
+}
+
+if (rex) {
+    rex.addEventListener("click", function() {
+        alert('Rączki mi już wysiadają! 😭');
+    });
+}
+
+if (bulwa) {
+    bulwa.addEventListener("click", function() {
+        alert('Pani Bulwa, pani Bulwa, pani Bulwa… Co, nie można pomarzyć?!');
+    });
+}
+
+if (czarny) {
+    czarny.addEventListener("click", function() {
+        alert('Ty druha we mnie masz! 🎤');
+    });
+}
